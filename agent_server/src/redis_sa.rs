1e03d909f829be3b7eb3f7fd90b99945c41ddc6d
why <awanghengyu@qq.com>
2021-08-13 15:41:21 +0800

    Init storage agent server
    The current version is suitable for stand-alone scenarios.
    In addition, the main problems that still exist at present include:
    1. The cache of authentication information cannot be synchronized between rpcs bacause each rpc has its own service instance, and the hashmap used for caching cannot be synchronized.
    2. Some error handling and temporary variable settings need to be improved.

---
+++ agent_server/src/redis_sa.rs
@@ -0,0 +1,138 @@
+use std::collections::HashMap;
+
+/*
+ * @Author: why
+ * @Date: 2021-08-03 21:38:29
+ * @LastEditTime: 2021-08-12 17:22:32
+ * @LastEditors: why
+ * @Description: 
+ * @FilePath: /sa/agent_server/src/redis_sa.rs
+ * 
+ */
+ 
+use redis::Commands;
+use crate::{faas_storage_agent::*};
+use crate::storage_ns::{Backend, BackendNamespace, Namespace};
+
+#[derive(PartialEq,Clone,Default,Debug)]
+pub struct Metadata {
+    user: String,
+    password: String,
+    endpoint: String,
+    port: u16
+}
+
+fn get_prefix(ns: &Namespace) -> String {
+    ns.client_id.to_string()
+}
+
+impl Metadata {
+    pub fn default() -> Metadata {
+        Metadata{
+            user: String::from("test"),
+            password: String::from("test"),
+            endpoint: String::from("redis://127.0.0.1"),
+            port: 6379
+        }
+    }
+    pub fn get_url(&self) -> String{
+        let url = format!("{}:{}", self.endpoint, self.port.to_string());
+        url
+    }
+}
+
+impl BackendNamespace for Metadata {
+    fn to_backend_ns_args(&self) -> Vec<(String, String)> {
+        let args: Vec<(String, String)> = vec![
+            ("user".to_string(), self.user.clone()),
+            ("password".to_string(), self.password.clone()),
+            ("endpoint".to_string(), self.endpoint.clone()),
+            ("port".to_string(), self.port.to_string()),
+            ];
+        args
+    }
+
+    fn from_hashmap_to_backend(map: &HashMap<String, String>) -> Backend {
+        let keys:[String; 4] = ["user".to_string(), "password".to_string(), "endpoint".to_string(), "port".to_string()];
+        let mut values = ["admin".to_string(),"admin".to_string(),"localhost".to_string(),"6379".to_string()];
+        let mut i = 0;
+
+        while i < 4 {
+            if let Some(v) = map.get(&keys[i]) {
+                values[i] = v.to_string();
+            }
+            i += 1;
+        }
+        
+        let md = Metadata{
+            user: values[0].to_string(),
+            password: values[1].to_string(),
+            endpoint: values[2].to_string(),
+            port: values[3].to_string().parse::<u16>().unwrap()
+        };
+        Backend::Redis(md)
+    }
+
+}
+
+fn get_metadata_from_ns(ns: Namespace) -> Metadata {
+    match ns.backend {
+        Backend::Redis(md) => md,
+    }
+}
+
+pub fn connect_ns(ns: Namespace) -> ns_resp {
+    let md = get_metadata_from_ns(ns);
+    let url = md.get_url();
+    let _client = redis::Client::open(url).unwrap();
+    let mut resp = ns_resp::default();
+    resp.set_err_code(0);
+    resp.set_err_info(String::from("connect successfully"));
+    resp
+}
+
+pub fn set(req: &data_req, _ns: Namespace) -> data_resp {
+    let md = Metadata::default();
+    let url = md.get_url();
+    let mut client = redis::Client::open(url).unwrap();
+    let _: () = client.set(get_prefix(&_ns) + req.get_key(), req.get_value()).unwrap();
+    let mut resp = data_resp::default();
+    resp.set_err_code(0);
+    resp.set_err_info(String::from("set successfully"));
+    resp
+}
+
+pub fn get(req: &data_req, _ns: Namespace) -> data_resp {
+    let md = Metadata::default();
+    let url = md.get_url();
+    let mut client = redis::Client::open(url).unwrap();
+    let value : String = client.get(get_prefix(&_ns) + req.get_key()).unwrap();
+    let mut resp = data_resp::default();
+    resp.set_err_code(0);
+    resp.set_value(value.into_bytes());
+    resp.set_err_info(String::from("get successfully"));
+    resp
+}
+
+pub fn exists(req: &data_req, _ns: Namespace) -> data_resp {
+    let md = Metadata::default();
+    let url = md.get_url();
+    let mut client = redis::Client::open(url).unwrap();
+    let rv: String = client.exists(get_prefix(&_ns) + req.get_key()).unwrap();
+    let mut resp = data_resp::default();
+    resp.set_err_code(0);
+    resp.set_value(rv.into_bytes());
+    resp.set_err_info(String::from("exists successfully"));
+    resp
+}
+
+pub fn delete(req: &data_req, _ns: Namespace) -> data_resp {
+    let md = Metadata::default();
+    let url = md.get_url();
+    let mut client = redis::Client::open(url).unwrap();
+    let _rv: String = client.del(get_prefix(&_ns) + req.get_key()).unwrap();
+    let mut resp = data_resp::default();
+    resp.set_err_code(0);
+    resp.set_err_info(String::from("delete successfully"));
+    resp
+}
