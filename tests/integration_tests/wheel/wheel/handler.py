c18040d1b68490cc272bbcf900e971c19727fe75
why <wang.hengyu@foxmail.com>
2021-09-19 09:45:29 -0700

    Add tests.

--- tests/integration_tests/wheel/wheel/handler.py
+++ tests/integration_tests/wheel/wheel/handler.py
@@ -7,73 +7,91 @@ def handle(req):
     rand = str(random.randint(1,1000))
     key = "test_key_" + rand
     value = ("test_value" + rand).encode()
-    count_ok = 10
+    test_count = 11
+    count_ok = test_count
 
     c, i = a.create_ns(ns_name)
     if c != 0:
         count_ok -= 1
         print("create_ns test ... failed. err_info:", i)
-    print("create_ns test ... ok")
+    else:
+        print("create_ns test ... ok")
     
     c, i = a.connect_ns(ns_name)
     if c != 0:
         count_ok -= 1
         print("connect_ns test ... failed. err_info:", i)
-    print("connect_ns test ... ok")
+    else:
+        print("connect_ns test ... ok")
 
     c, i = a.set(key, value)
     if c != 0:
         count_ok -= 1
         print("set test ... failed. err_info:", i)
-    print("set test ... ok")
+    else:
+        print("set test ... ok")
 
     c, i = a.exists(key)
     if c != 0:
         count_ok -= 1
         print("exists test ... failed. err_info:", i)
-    print("exists test ... ok")
+    else:
+        print("exists test ... ok")
 
     c, i, v = a.get(key)
     if c != 0:
         print("get test ... failed. err_info:", i)
-    if v != value:
+    elif v != value:
         count_ok -= 1
         print("get test ... failed. err value : ", v.decode())
-    print("get test ... ok")
+    else:
+        print("get test ... ok")
 
     c, i = a.delete(key)
     if c != 0:
         count_ok -= 1
         print("delete test ... failed. err_info:", i)
-    print("delete test ... ok")
-
+    else:
+        print("delete test ... ok")
+        
     c, i = a.exists(key)
     if c == 0:
         count_ok -= 1
         print("exists test ... failed. err_info:", i)
-    print("exists test ... ok")
+    else:
+        print("exists test ... ok")
 
     c, i, v = a.get(key)
     if c == 0:
         count_ok -= 1
         print("get test ... failed. err_info:", i)
-    print("get test ... ok")
+    else:
+        print("get test ... ok")
 
     c, i = a.delete_ns(ns_name)
     if c != 0:
         count_ok -= 1
         print("delete_ns test ... failed. err_info:", i)
-    print("delete_ns test ... ok")
+    else:
+        print("delete_ns test ... ok")
 
     c, i = a.connect_ns(ns_name)
     if c == 0:
         count_ok -= 1
         print("connect_ns test ... failed. err_info:", i)
-    print("connect_ns test ... ok")
+    else:
+        print("connect_ns test ... ok")
     
-    if count_ok == 10 :
+    c, i = a.delete_ns(ns_name)
+    if c == 0:
+        count_ok -= 1
+        print("delete_ns test ... failed. err_info:", i)
+    else:
+        print("delete_ns test ... ok")
+
+    if count_ok == test_count :
         res = "ok"
     else :
         res = "failed"
-    print("test result: {0}. {1} passed; {2} failed", res, count_ok, 10 - count_ok)
+    print("test result: {0}. {1} passed; {2} failed".format(res, count_ok, test_count - count_ok))
     return req
